705b42272cd74a2804d07c114c79ea88ef82e77f
Samuel Ajibade <samuelajibade22@gmail.com>
2021-04-08 21:54:01 +0100

    created Navigation component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">AjibsBaba</a>
+    </div>
+  </nav>
+</div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
