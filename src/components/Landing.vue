e7967c588e186a7e76eee1021a40f4de00c1d737
Samuel Ajibade <samuelajibade22@gmail.com>
2021-04-24 21:58:05 +0100

    added content to components

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,6 +1,10 @@
 <template>
-<div>
-
+<div class="container">
+  <div class="position-absolute top-50 translate-middle-y">
+    <h1 class="text-wrap" style="width: 22rem">Portfolio Of Samuel Ajibade.</h1>
+    <h2 class="mt-2 text-wrap" style="width: 18rem">Backend , Native Android, Data Analytics, API development, Deep Learning.</h2>
+    <button class="btn btn-warning text-white mt-4">Resume</button>
+  </div>
 </div>
 </template>
 
@@ -11,5 +15,27 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  font-family: 'Poppins', sans-serif;
+  font-weight: lighter;
+  color: #353535;
+  font-size: small;
+  opacity: 71%;
+}
+
+h1 {
+  font-family: 'Poppins', sans-serif;
+  font-weight: bold;
+  color: #3B3B3B;
+}
+
+.btn-warning {
+  background-color: #FFB037;
+  font-family: 'Poppins', sans-serif;
+  font-weight: bold;
+  width: 170px;
+  height: 52px;
+  font-size: 14px;
+}
 
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,29 @@
 <template>
 <div>
-  <nav class="navbar navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-trans">
     <div class="container">
-      <a class="navbar-brand">AjibsBaba</a>
+      <a class="navbar-brand" href="">
+        <img src="../assets/Lomo.svg" alt="logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Projects</a>
+          </li>
+        </ul>
+      </div>
+      <form class="d-flex">
+        <button class="btn btn-warning text-white">Contact</button>
+      </form>
     </div>
   </nav>
 </div>
