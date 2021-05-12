ed4bfcc0bb7bc9e8484fc6e7b56ce1e66b7d3aa9
Samuel Ajibade <samuelajibade22@gmail.com>
2021-05-12 21:34:42 +0100

    removed navbar for mobile and desktop view

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,10 @@
 <template>
 <div>
-  <nav class="navbar navbar-expand-lg navbar-light bg-trans mt-4">
+  <nav class="navbar navbar-expand-lg navbar-light bg-trans">
     <div class="container">
       <a class="navbar-brand" href="">
         <img src="../assets/Lomo.svg" alt="logo" width="68px" height="46px">
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Projects</a>
-          </li>
-        </ul>
-      </div>
-<!--      <form class="d-flex">-->
-<!--        <button class="btn btn-warning btn-lg text-white">Contact</button>-->
-<!--      </form>-->
     </div>
   </nav>
 </div>
