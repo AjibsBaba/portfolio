a7deed81e6f8e5c273e33b79e0733d6067abc27a
Samuel Ajibade <samuelajibade22@gmail.com>
2021-05-13 15:00:56 +0100

    added css styling added contents made design responsive for mobile view improved accessibility

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,35 @@
 <template>
+  <!-- Footer -->
+  <footer class="footer-margin yellow p-1">
+    <div class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col">
+            <h1 class="mt-3 text-white">About</h1>
+            <h4 class="mt-2 text-wrap" style="width: 9rem">Designed and developed with with Vue , Nuxt Js, CSS and Figma</h4>
+            <h4 class="mt-2">developerLomo 2021</h4>
+          </div>
+          <div class="col">
+            <h1 class="mt-3 text-white">Explore</h1>
+            <h4 class="mt-2">Home</h4>
+            <h4 class="mt-2">About</h4>
+            <h4 class="mt-2">Projects</h4>
+          </div>
+          <div class="col">
+            <h1 class="mt-3 text-white">Social</h1>
+            <a href="#" class="fad-pad" target="_blank"><i class="fab fa-twitter"></i></a>
+            <i class="fab fa-github fad-pad"></i>
+            <i class="fab fa-instagram fad-pad"></i>
+          </div>
+        </div>
 
+      </div>
+      </div>
+      <!-- Grid row -->
+
+
+  </footer>
+  <!-- Footer -->
 </template>
 
 <script>
@@ -9,5 +39,35 @@ export default {
 </script>
 
 <style scoped>
+.footer-margin {
+  margin-top: 25px;
+}
+.fad-pad {
+  margin-right: 5px;
+  color: #FFFFFF;
+  opacity: 0.7;
+}
+.fad-pad:hover {
+  color: white;
+}
+
+h1 {
+  font-family: 'Poppins', sans-serif;
+  font-weight: bold;
+  font-size: 24px;
+}
+h4 {
+  font-family: 'Poppins', sans-serif;
+  font-size: 8px;
+  font-weight: 400;
+  font-style: normal;
+  color: rgba(255, 255, 255, 0.7);
+}
+.yellow {
+  background-color: #FFB037;
+  margin-top: 30px;
+}
+
+
 
 </style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,23 +2,54 @@
 <div class="container">
   <div class="row">
     <div class="col port-col">
-      <div class="mt-custom">
-        <h1 class="text-wrap" style="width: 22rem">Portfolio Of Samuel Ajibade.</h1>
-        <h2 class="text-wrap" style="width: 25rem">Backend , Native Android, Data Analytics, API development, Deep Learning.</h2>
+      <div class="mt-custom ms-4">
+        <h1 class="user-select-none">Portfolio Of Samuel Ajibade.</h1>
+        <h2 class="user-select-none">Backend , Native Android, API development, Deep Learning & some frontend things like VueJs</h2>
         <a href="https://drive.google.com/file/d/1eqvXKmCCloOJTI4T0RV6Wk-ISfO2yR_4/view?usp=sharing" target="_blank" download>
           <button class="btn btn-warning" type="button">Resume</button>
         </a>
       </div>
     </div>
-    <div class="col vec-col">
-      <div class="img-block">
+    <div class="col vec-col d-none d-lg-block">
+      <div class="img-block mt-4">
         <img src="../assets/vector.svg" alt="Man Showing analytics" >
       </div>
     </div>
   </div>
-  <div class="row justify-content-md-center">
-    <div class="col-md-auto nav-btn">
+    <div class="nav-btn">
       Scroll Down
+    </div>
+  <div class="row mt-3" style="background-color: #FFB037">
+    <div class="col-sm-6 align-self-center">
+      <div class="p-4">
+        <h1 class="name-tag">Hi, I'm Samuel</h1>
+        <h3 class="mt-2 position-tag">Software Engineer</h3>
+        <p class="mt-3 info">A software engineer with hands-on experience in developing web and mobile applications using different architectures.
+          I currently work with Django, Vue & Kotlin</p>
+      </div>
+    </div>
+    <div class="col-sm-6 align-self-center order-first p-4">
+      <div class="text-center">
+        <img src="../assets/passport.jpg" class="img-fluid rounded" alt="Samuel Ajibade's Image">
+      </div>
+    </div>
+  </div>
+  <div class="row mt-4 ms-1">
+    <div class="col">
+      <h1 class="main">Projects</h1>
+      <h3 class="mt-1 subtitle">Most Recent Works</h3>
+    </div>
+    <div class="row">
+      <div class="col-sm-6 align-self-center order-first p-4">
+        <div class="text-center">
+          <img src="../assets/diabetes.png" alt="Product info" class="img-fluid rounded">
+        </div>
+      </div>
+      <div class="col-sm-6 align-self-center order-first p-4">
+        <h3 class="product-name">diabetes-diagnosis</h3>
+        <p class="product-info">predicts diabetes mellitus type 2 with the aid of machine learning. Written in Flask, Html, CSS.</p>
+        <h5 class="link-custom">Visit Website</h5>
+      </div>
     </div>
   </div>
 
@@ -32,6 +63,64 @@ export default {
 </script>
 
 <style scoped>
+.product-name {
+  font-family: 'Poppins', sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  color: #3B3B3B;
+}
+.product-info {
+  font-family: 'Poppins', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  color: rgba(53, 53, 53, 0.71);
+}
+.link-custom {
+  color: #FFB037;
+  font-family: 'Poppins', sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  size: 16px;
+}
+.info {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 20px;
+  color: #FFFFFF;
+}
+.position-tag {
+  color: #FFFFFF;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  font-size: 24px;
+  opacity: 0.7;
+}
+.name-tag {
+  color: white;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  size: 40px;
+}
+.main {
+  font-family: 'Poppins', sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  size: 48px;
+  color: #3B3B3B;
+}
+.subtitle {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 800;
+  font-style: normal;
+  size: 18px;
+  color: #B2B0B0;
+}
+
 .img-block {
   text-align: center;
   display: block;
@@ -46,7 +135,9 @@ export default {
   font-size: 16px;
   font-family: 'Poppins', sans-serif;
   font-weight: normal;
-  margin-top: 15px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
 }
 .port-col {
   background-color: rgba(199, 199, 199, 0.1);
@@ -93,8 +184,5 @@ h1 {
   color: #FFB037;
 }
 
-.sec {
-  background-color: #FFB037;
-}
 
 </style>
