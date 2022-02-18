ed3c594bc2f4b6b960861ca159ee392f95384863
Samuel Ajibade <samuelajibade22@gmail.com>
2022-02-18 09:33:23 +0100

    updated SEO

--- src/App.vue
+++ src/App.vue
@@ -17,18 +17,29 @@ export default {
   name: 'App',
   metaInfo: {
     title: "Portfolio | Samuel Ajibade",
+    htmlAttrs: {
+      lang: 'en'
+    },
     meta: [
       {charset: "utf-8"},
-      {name: "og:title", content: "Portfolio | Samuel Ajibade"},
-      {name: "og:url", content: "https://www.ajibsbaba.com"},
-      {name: "og:type", content: "website"},
-      {name: "og:image", content: "https://www.ajibsbaba.com/img/passport.1fa0f126.png"},
-      {name: "og:description", content: "A Software Engineering Portfolio of Samuel Ajibade"},
-      {name: "description", content: "A Software Engineering Portfolio of Samuel Ajibade"},
-      {name: "theme-color", content: "#FFB037"},
-      {name: "msapplication-navbutton-color", content: "#FFB037"},
-      {name: "apple-mobile-web-app-status-bar-style", content: "#FFB037"},
-      {name: "keyword", content: "Samuel Ajibade, Ajibade, developerlomo, lomo, Portfolio, Software Engineer, Web Developer, Mobile Engineer, Android Developer, Developer, Frontend Developer"}
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { name: "og:title", content: "Portfolio | Samuel Ajibade"},
+      { name: "og:url", content: "https://www.ajibsbaba.com"},
+      { name: "og:type", content: "website"},
+      { name: "og:image", content: "https://www.ajibsbaba.com/img/passport.1fa0f126.png"},
+      { name: 'og:url', content: 'https://study-mate.netlify.app/'},
+      { name: 'og:site_name', content: 'Portfolio | Samuel Ajibade' },
+      { name: 'twitter:title', content: 'Portfolio | Samuel Ajibade' },
+      { name: 'twitter:description', content: 'A Software Engineering Portfolio of Samuel Ajibade'},
+      { name: 'twitter:site', content: '@ajibs_baba'},
+      { name: 'twitter:creator', content: '@ajibs_baba'},
+      { name: "og:description", content: "A Software Engineering Portfolio of Samuel Ajibade"},
+      { name: "description", content: "A Software Engineering Portfolio of Samuel Ajibade"},
+      { name: "theme-color", content: "#FFB037"},
+      { name: "msapplication-navbutton-color", content: "#FFB037"},
+      { name: "apple-mobile-web-app-status-bar-style", content: "#FFB037"},
+      { name: 'robots', content: 'index, follow'},
+      { name: "keyword", content: "Samuel Ajibade, Ajibade, developerlomo, lomo, Portfolio, Software Engineer, Web Developer, Mobile Engineer, Android Developer, Developer, Frontend Developer"}
     ]
   },
   components: {
